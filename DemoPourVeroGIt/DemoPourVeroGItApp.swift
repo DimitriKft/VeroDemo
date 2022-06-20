fbd9c5824b43854a634f8f2b4578d7504eca9870
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-06-20 15:09:20 +0200

    Coucou Vero

---
+++ DemoPourVeroGIt/DemoPourVeroGItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPourVeroGItApp.swift
+//  DemoPourVeroGIt
+//
+//  Created by dimitri on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoPourVeroGItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
